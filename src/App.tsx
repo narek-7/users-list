84c0127683ed61ced7a1ec95bb5e0c7da17f6b96
Narek <narek1985ch@gmail.com>
2022-05-19 20:39:46 +0200

    added component NotFound and Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import UsersList from "./components/usersList";
+import User from "./components/user";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFound from "./components/notFound";
 
 const App = () => {
-   return <div></div>;
+   return (
+      <Switch>
+         <Route path="/users" exact component={UsersList} />
+         <Route path="/users/:userId?" component={User} />
+         <Route path="/404" component={NotFound} />
+         <Redirect from="/" to="/users" />
+         <Route path="/" exact component={UsersList} />
+         <Redirect to="/404" />
+      </Switch>
+   );
 };
 
 export default App;
